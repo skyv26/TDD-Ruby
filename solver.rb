1f1acdb6a20f12062dd6f4da893544692af3c602
skyv26 <dev.aakashv@gmail.com>
2022-11-19 01:58:09 +0530

    Add the exception to `solver` file

--- solver.rb
+++ solver.rb
@@ -1,7 +1,14 @@
-class Solver
+require_relative 'exception_handler'
 
+class Solver
+  def initialize
+    @exception = ExceptionHandler.new
+  end
   def factorial(n)
-    if(n == 1)
+    if(n < 0)
+      @exception.msg('factorial')
+    end
+    if(n == 1 || n == 0)
       return n
     end
     n * factorial(n-1)
