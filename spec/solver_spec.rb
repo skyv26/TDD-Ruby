603f20ca7828c75bfde9b93ceea0ca94d173e66c
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-11-18 23:02:33 +0300

    Updates to solver test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -15,6 +15,19 @@ describe Solver do
         expect(@solver.factorial(5)).to eql(120)
       end
 
+      it 'will test the `factorial` of 0 should be 0' do
+        expect(@solver.factorial(0)).to eql(0)
+      end
+
+      it 'will test the `factorial` of 1 should be 1' do
+        expect(@solver.factorial(1)).to eql(1)
+      end
+    end
+
+    describe 'Testing factorial method with Exception' do
+      it 'will test the `factorial` of negative number should raise error' do
+        expect {@solver.factorial(1)}.to raise_error(RuntimeError)
+      end
     end
   end
 end
