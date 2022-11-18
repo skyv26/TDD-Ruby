eab7ebaee99ce55d58b28b76eb5a15a723fd7c78
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-11-18 22:34:08 +0300

    Update solver test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,13 @@
 describe Solver do
   context '#solver' do
-    @solver = Solver.new
+    before do
+      @solver = Solver.new
+    end
+
+    describe 'Testing factorial method' do
+      it 'will test the `factorial` of 3 should be 6' do
+        expect(@solver.factorial(3)).to eql(6)
+      end
+    end
   end
 end
