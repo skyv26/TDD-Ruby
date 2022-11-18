9d3d8b3a889aaf1062fa31393e56f395e5ffdce3
skyv26 <dev.aakashv@gmail.com>
2022-11-19 02:30:54 +0530

    Resolv the `linter` errors

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -14,9 +14,6 @@ describe Solver do
       it 'will test the `factorial` of 0 should be 0' do
         expect(@solver.factorial(0)).to eql(0)
       end
-      it 'will test the `factorial` of 1 should be 1' do
-        expect(@solver.factorial(1)).to eql(1)
-      end
       #   it 'will test the `factorial` of negative number should raise error' do
       #     expect {@solver.factorial(1)}.to raise_error(RuntimeError)
       #   end
@@ -41,11 +38,10 @@ describe Solver do
       it 'will test method and for 15 resultant should be "fizz"' do
         expect(@solver.fizzbuzz(15)).to eql('fizzbuzz')
       end
-      
+
       it 'will test method and for 7 resultant should be "7"' do
         expect(@solver.fizzbuzz(7)).to eql('7')
       end
-
     end
   end
 end
